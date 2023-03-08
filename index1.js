cd4591894a70e92f5978c8041930146b37de73ec
KY <nguyenhoangky23121997@gmail.com>
2023-03-08 16:41:41 +0700

    commit0

--- index1.js
+++ index1.js
@@ -1,5 +1,12 @@
+function nooption(){
+    x = document.getElementById("gow").getAttribute("src")
+    y = x.charAt(0);
+    y = +y + 1;
+    if(y>7){
+        y=1
+    }
+    x = y +x.substring(1);
 
+    document.getElementById("gow").src = x;
 
-function dosomething() {
-    document.getElementById("showhello").innerHTML="Hello"
 }
